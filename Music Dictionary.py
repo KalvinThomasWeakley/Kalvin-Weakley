11d562cbe71d2f3005296489e47b796c4ab7d8af
Kalvin Thomas boone Weakley <kalvin.weakley@stu.jefferson.kyschools.us>
2018-03-16 09:28:55 -0400

    Second Commit.

---
+++ Music Dictionary.py
@@ -0,0 +1,13 @@
+band_terms = {
+    'Tempo:': "The tempo is the speed in which the music is set in. Most commonly written in BPM/Beats Per Minute.",
+    'Dynamic:': "The dynamic is the volume of the music, ranging from Pianississimo (very very quiet) to Fortississimo (very very loud).",
+    'Crescendo/ Decrescendo:': "To gradually get louder/ to gradually get quieter.",
+    'Soprano:': "The highest voice out of the four (Soprano, Alto, Tenor, Bass) in music.",
+    'Bass:': "The lowest voice out of the four (Soprano, Alto, Tenor, Bass)",
+    'Movement:': "A section of a piece of music the splits it apart from others or tends to have its own style. Typically four movements.",
+    }
+for k, v in band_terms.items():
+    print (k, v)
+    
+
+
